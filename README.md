98bc91de5699ce3bbaf925478b3bacdeefa0c941
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-04-26 17:01:54 +0200

    tests: scenario 1 completed

--- README.md
+++ README.md
@@ -14,15 +14,6 @@ Actions:
 TODO:
 - Tests
 
-## Tests
-
-Scenarios:
-- There's a very small amount of ETH in the contract to claim for the previous period (like just a few wei or even just 1 wei). What happens if there are multiple stakers with different staked amounts? What happens if minUserDeposit and claimRewardsMinimum are set too low (1 wei or even 0)?
-- User tries to claim for another user. Should be possible, but the asset owner should always receive the rewards, no one else.
-- User makes a huge deposit right before the previous claim period ends. And then makes the claim right after the new claim period starts. And then tries to withdraw assets. (Should not be possible because of the staking lock which lasts as long as one period)
-- User sends receipt tokens to another address before claiming
-- User sends receipt tokens to another address after claiming
-
 ## Use at your own risk
 
 The contracts have not been audited. Use at your own risk.
--- test/periodicEthRewards.test.js
+++ test/periodicEthRewards.test.js
@@ -30,7 +30,7 @@ describe("PeriodicEthRewards", function () {
   const user1stakingTokenBalance = ethers.utils.parseEther("850");
   const user2stakingTokenBalance = ethers.utils.parseEther("1500");
 
-  const claimRewardsMinimum = ethers.utils.parseEther("10");
+  const claimRewardsMinimum = ethers.utils.parseEther("1");
   const minUserDeposit = ethers.utils.parseEther("0.0001");
   const claimPeriod = 604800; // 1 week
 
@@ -46,7 +46,7 @@ describe("PeriodicEthRewards", function () {
       stakingTokenContract.address,
       "Receipt Token",
       "RCP",
-      claimRewardsMinimum, // 10 ETH as minimum rewards total per period
+      claimRewardsMinimum, // 1 ETH as minimum rewards total per period
       minUserDeposit, // 0.0001 ETH as minimum user deposit
       claimPeriod // 1 week claim period
     );
@@ -57,9 +57,9 @@ describe("PeriodicEthRewards", function () {
     await stakingTokenContract.mint(user2.address, user2stakingTokenBalance); // 1500 tokens
   });
 
-  // scenario 1: user1 deposits 100 tokens, user2 deposits 200 tokens
+  // scenario 1: user1 deposits 300 tokens, user2 deposits 700 tokens
   // user1 should get 30% of the rewards and user2 should get 70% of the rewards after 1 week
-  it("should distribute rewards correctly", async function () {
+  it("Scenario 1: user1 deposits 300 tokens, user2 deposits 700 tokens. They claim rewards after 1 week.", async function () {
     const user1tokensToDeposit = ethers.utils.parseEther("300");
     const user2tokensToDeposit = ethers.utils.parseEther("700");
 
@@ -84,7 +84,7 @@ describe("PeriodicEthRewards", function () {
     // check rewards contract balance before
     expect(await ethers.provider.getBalance(rewardsContract.address)).to.equal(0);
 
-    // send 10 ETH to the rewards contract
+    // send 9 ETH to the rewards contract
     await owner.sendTransaction({ 
       value: ethers.utils.parseEther("9"),
       to: rewardsContract.address 
@@ -97,7 +97,7 @@ describe("PeriodicEthRewards", function () {
     await ethers.provider.send("evm_increaseTime", [604801]); // 1 week + 1 second
     await ethers.provider.send("evm_mine");
 
-    // send some more ETH to the rewards contract to trigger _updateLastClaimPeriod
+    // send 1 more ETH to the rewards contract to trigger _updateLastClaimPeriod
     // this ETH will be added to the rewards pool for the previous claim period, so 10 ETH in total (9 ETH + 1 ETH)
     await owner.sendTransaction({ 
       value: ethers.utils.parseEther("1"),
@@ -107,12 +107,65 @@ describe("PeriodicEthRewards", function () {
     // check rewards contract balance
     expect(await ethers.provider.getBalance(rewardsContract.address)).to.equal(ethers.utils.parseEther("10"));
 
-    // user1 should have 30% of the rewards
+    // user1 can have 30% of the rewards
     expect(await rewardsContract.connect(user1).previewClaim(user1.address)).to.equal(ethers.utils.parseEther("3"));
 
-    // user2 should have 70% of the rewards
+    // user2 can have 70% of the rewards
     expect(await rewardsContract.connect(user2).previewClaim(user2.address)).to.equal(ethers.utils.parseEther("7"));
 
-    // todo: claim rewards
+    // preview claim for user3 (should be 0)
+    expect(await rewardsContract.connect(user3).previewClaim(user3.address)).to.equal(0);
+
+    // user1 ETH balance before
+    const user1BalanceBefore = await ethers.provider.getBalance(user1.address);
+    console.log("user1 ETH balance before: ", ethers.utils.formatEther(user1BalanceBefore));
+
+    // owner claims rewards for user1 (so that no gas fees are paid by user1)
+    await rewardsContract.claimRewardsFor(user1.address);
+
+    // user1 ETH balance after
+    const user1BalanceAfter = await ethers.provider.getBalance(user1.address);
+    console.log("user1 ETH balance after: ", ethers.utils.formatEther(user1BalanceAfter));
+    expect(user1BalanceAfter).to.equal(user1BalanceBefore.add(ethers.utils.parseEther("3")));
+
+    // user2 ETH balance before
+    const user2BalanceBefore = await ethers.provider.getBalance(user2.address);
+    console.log("user2 ETH balance before: ", ethers.utils.formatEther(user2BalanceBefore));
+
+    // user2 claims rewards for themselves
+    const tx = await rewardsContract.connect(user2).claimRewards();
+    const receipt = await tx.wait();
+    calculateGasCosts("user claimRewards()", receipt);
+
+    // user2 ETH balance after
+    const user2BalanceAfter = await ethers.provider.getBalance(user2.address);
+    console.log("user2 ETH balance after: ", ethers.utils.formatEther(user2BalanceAfter));
+    // expect user2 balance before to be less than user2 balance after
+    expect(user2BalanceBefore).to.be.lt(user2BalanceAfter);
+
+    // check rewards contract balance (should be 0 ETH)
+    expect(await ethers.provider.getBalance(rewardsContract.address)).to.equal(0);
+
+    // user3 ETH balance before
+    const user3BalanceBefore = await ethers.provider.getBalance(user3.address);
+    console.log("user3 ETH balance before: ", ethers.utils.formatEther(user3BalanceBefore));
+
+    // owner tries to claim rewards for user3 (should not fail, but should return 0 instead)
+    await rewardsContract.claimRewardsFor(user3.address);
+
+    // user3 ETH balance after (should be the same as before)
+    const user3BalanceAfter = await ethers.provider.getBalance(user3.address);
+    console.log("user3 ETH balance after: ", ethers.utils.formatEther(user3BalanceAfter));
+    expect(user3BalanceAfter).to.equal(user3BalanceBefore);
   });
+
+  // Scenario 2: there's a very small amount of ETH in the rewards contract (just 1 wei). claimRewardsMinimum needs to be set to 0.
+
+  // Scenario 3: user tries to withdraw their staking tokens before the asset lock period is over
+
+  // Scenario 4: user sends receipt tokens to another address. What happens to the rewards? (both addresses try to claim rewards)
+
+  // Scenario 5: rewards amount is lower than claimRewardsMinimum. What happens to rewards? Can users claim?
+
+  // Scenario 6: minUserDeposit is set to 1 wei, claimRewardsMinimum is set to 0.0001 ETH. User1 deposits 1 wei, user2 deposits 10 ETH. The reward is 0.001 ETH. How much does each user get?
 });
